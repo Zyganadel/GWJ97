5d5018ec482c1bfac40440140ea03902e036bb96
nullkia <nullkia@stellarforgeinteractive.ca>
2026-09-12 19:23:53 -0300

    Added power component

---
+++ src/Player/PowerComponent.cs
@@ -0,0 +1,64 @@
+using System;
+using Godot;
+
+namespace GWJ97.Player;
+
+public partial class PowerComponent : Node
+{
+    // -- Events --
+    public event Action Overpowered;
+
+    // -- Export --
+    /// <summary>
+    /// The maximum power the player can hold before they die.
+    /// </summary>
+    [Export] int maxPower = 5;
+
+    /// <summary>
+    /// The number of physics ticks it should take to lose a power level.
+    /// </summary>
+    [Export] int decayRate = 600;
+
+    // -- Prop --
+    /// <summary>
+    /// The player's power level
+    /// </summary>
+    public int PowerLevel
+    {
+        get => powerLevel;
+        private set => powerLevel = int.Max(value, 0);
+    }
+    
+    // -- Backers --
+    int powerLevel = 0;
+
+    // -- Field --
+    int ticks;
+
+    // -- Override --
+    public override void _PhysicsProcess(double delta)
+    {
+        CheckPower();
+    }
+
+    // -- Methods --
+    
+    /// <summary>
+    /// Raises the power level by 1.
+    /// </summary>
+    public void BumpPower()
+    {
+        powerLevel++;
+        ticks = 0;
+        if (powerLevel > maxPower) Overpowered?.Invoke();
+    }
+
+    // Check how long its been since we updated power level. if greater than decay rate, drop power by 1.
+    void CheckPower()
+    {
+        ticks++;
+        if (ticks < decayRate) return;
+        ticks = 0;
+        powerLevel--;
+    }
+}
