158907749679006f233089ec90161311b39d53d7
nullkia <nullkia@stellarforgeinteractive.ca>
2026-09-12 21:02:47 -0300

    Add hurtbox/hitbox

---
+++ src/Damage/Hitbox.cs
@@ -0,0 +1,27 @@
+using Godot;
+
+namespace GWJ97.Damage;
+
+/// <summary>
+/// Represents a thing that hurts.
+/// </summary>
+public partial class Hitbox:Area3D
+{
+    /// <summary>
+    /// Used to prevent friendly fire. Assumes player will be 0, foes will be 1.
+    /// See also <see cref="Hurtbox.team"/>
+    /// </summary>
+    [Export] public int team=1;
+
+    public override void _Ready()
+    {
+        AreaEntered += OnAreaEntered;
+    }
+
+    void OnAreaEntered(Area3D area)
+    {
+        if (area is not Hurtbox target) return;
+        if (target.team == team) return;
+        target.TakeHit();
+    }
+}
---
+++ src/Damage/Hurtbox.cs
@@ -0,0 +1,26 @@
+using System;
+using Godot;
+
+namespace GWJ97.Damage;
+
+/// <summary>
+/// Represents a thing that can be hurt.
+/// </summary>
+public partial class Hurtbox:Area3D
+{
+    public event Action HitTaken;
+    
+    /// <summary>
+    /// Used to prevent friendly fire. Assumes player will be 0, foes will be 1.
+    /// See also <see cref="Hitbox.team"/>
+    /// </summary>
+    [Export] public int team=1;
+
+    /// <summary>
+    /// Causes this hurtbox to take damage.
+    /// </summary>
+    public void TakeHit()
+    {
+        HitTaken?.Invoke();
+    }
+}
