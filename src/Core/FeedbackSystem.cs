fa903ff834eaa1c125a08b35335941db70c08f14
nullkia <nullkia@stellarforgeinteractive.ca>
2026-09-14 17:34:06 -0300

    why did i need a todo for that

--- src/Core/FeedbackSystem.cs
+++ src/Core/FeedbackSystem.cs
@@ -4,6 +4,7 @@ namespace GWJ97.Core;
 
 public partial class FeedbackSystem:Node
 {
+    // -- export --
     [Export] PackedScene foeHitScene, foeDeathScene, playerPowerUpScene, playerHitScene;
 
     // -- overrides --
@@ -21,7 +22,7 @@ public partial class FeedbackSystem:Node
     void SpawnThingy(PackedScene bla, Vector3 pos = new())
     {
         Node bla2 = bla.Instantiate();
-        // todo: hierarchy stuff go here
+        AddChild(bla2);
         if (bla2 is Node3D bla3)
         {
             bla3.Position = pos;
