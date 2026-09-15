fcd924f77392aae5d42f47740026f55d5f4fe060
Joshua Toohey <tooheyjosh@outlook.com>
2026-09-15 00:48:20 -0300

    Player Movement Baseline

--- src/Scenes/Entities/Player.cs
+++ src/Scenes/Entities/Player.cs
@@ -3,26 +3,44 @@ using System;
 
 public partial class Player : Node3D
 {
-    [Export] private double maxSpeed;
+    [Export] private float moveSpeed;
     [Export] private double acceleration;
 
     private Vector3 velocity;
 
-    private void movement(double delta) {
-        Vector2 movementInput = Input.GetVector("Left", "Right", "Forward", "Backward");
-        this.velocity = new Vector3(this.velocity.X + (float)(movementInput.X * acceleration * delta), this.velocity.Y, this.velocity.Z + (float)(movementInput.Y * acceleration * delta));
-        if (Vector2(this.velocity.X, this.velocity.Z).Length() > maxSpeed) {
-
-        }
-        this.Position = new Vector3(this.Position.X + (float)(movementInput.X * maxSpeed * delta), this.Position.Y, this.Position.Z + (float)(movementInput.Y * maxSpeed * delta));
-    }
-    public override void _Input(InputEvent @event) {
-        if (@event is InputEventKey keyEvent && keyEvent.Pressed) {
-            switch (keyEvent.Keycode) {
-                case Key.W:
-                break;
+    private float velocityChangeCalc(float movementInput, float currentVelocity, double delta) {
+        float velocity = 0;
+        if (movementInput != 0)
+        {
+            int turning = 1;
+            if (Math.Sign(movementInput) != Math.Sign(currentVelocity)) {
+                turning = 2;
+            }
+            velocity = currentVelocity + (float)(movementInput * acceleration * delta * turning);
+            if (Math.Abs(velocity) > (Math.Abs(movementInput) * moveSpeed))
+            {
+                velocity = movementInput * moveSpeed;
             }
         }
+        else if (currentVelocity != 0)
+        {
+            velocity = currentVelocity - (float)(acceleration * delta * Math.Sign(currentVelocity));
+            if (Math.Abs(velocity) < (acceleration * delta))
+            {
+                velocity = 0;
+            }
+        }
+        return velocity;
+    }
+
+    private void movement(double delta) {
+        float velocityX;
+        float velocityZ;
+        Vector2 movementInput = Input.GetVector("Left", "Right", "Forward", "Backward");
+        velocityX = velocityChangeCalc(movementInput.X, this.velocity.X, delta);
+        velocityZ = velocityChangeCalc(movementInput.Y, this.velocity.Z, delta);
+        this.velocity = new Vector3(velocityX, this.velocity.Y, velocityZ);
+        this.Position = new Vector3(this.Position.X + (float)(velocity.X * delta), this.Position.Y + (float)(velocity.Y * delta), this.Position.Z + (float)(velocity.Z * delta));
     }
     public override void _Ready()
     {
