5ab64c4e955f1dc3665464379397f0cea97f0e69
Twoej <tooheyjosh@outlook.com>
2026-09-20 02:23:24 -0300

    Added Idle animations

--- src/Scenes/Entities/Player.cs
+++ src/Scenes/Entities/Player.cs
@@ -20,6 +20,8 @@ public partial class Player : Node3D
 
     private bool attacking = false;
 
+    private AnimatedSprite3D sprite;
+
     //Runs twice per frame for the X and Z axis velocities. calculates velocity based on previous frame and current buttons pressed.
     private float velocityChangeCalc(float movementInput, float currentVelocity, double delta)
     {
@@ -48,6 +50,14 @@ public partial class Player : Node3D
         return velocity;
     }
 
+    private void playCurrentIdleAnimation() {
+        if (this.velocity.Length() == 0) {
+            return;
+        }
+        String animation = sprite.Animation + " Idle";
+        sprite.Play(animation);
+    }
+
     private void movement(double delta) {
         float velocityX;
         float velocityZ;
@@ -59,7 +69,7 @@ public partial class Player : Node3D
         velocityZ = velocityChangeCalc(movementInput.Y, this.velocity.Z, delta);
         if (velocityX == 0 && velocityZ == 0)
         {
-            GetNode<AnimatedSprite3D>("AnimatedSprite3D").Stop();
+            playCurrentIdleAnimation();
         }
         this.velocity = new Vector3(velocityX, this.velocity.Y, velocityZ);
         this.Position = new Vector3(this.Position.X + (float)(velocity.X * delta), this.Position.Y + (float)(velocity.Y * delta), this.Position.Z + (float)(velocity.Z * delta));
@@ -69,6 +79,7 @@ public partial class Player : Node3D
         this.velocity = new Vector3(0, 0, 0);
         hurtbox = GetNode<Hurtbox>("Hurtbox");
         hurtbox.HitTaken += hurtboxOnHitTaken;
+        sprite = GetNode<AnimatedSprite3D>("AnimatedSprite3D");
     }
     public override void _Process(double delta)
     {
@@ -77,21 +88,21 @@ public partial class Player : Node3D
     public override void _Input(InputEvent @event) {
         if (@event.IsActionPressed("Forward"))
         {
-            GetNode<AnimatedSprite3D>("AnimatedSprite3D").Play("Forward");
+            sprite.Play("Forward");
         }
         if (@event.IsActionPressed("Backward"))
         {
-            GetNode<AnimatedSprite3D>("AnimatedSprite3D").Play("Backward");
+            sprite.Play("Backward");
         }
         if (@event.IsActionPressed("Right"))
         {
-            GetNode<AnimatedSprite3D>("AnimatedSprite3D").FlipH = false;
-            GetNode<AnimatedSprite3D>("AnimatedSprite3D").Play("Side");
+            sprite.FlipH = false;
+            sprite.Play("Side");
         }
         if (@event.IsActionPressed("Left"))
         {
-            GetNode<AnimatedSprite3D>("AnimatedSprite3D").FlipH = true;
-            GetNode<AnimatedSprite3D>("AnimatedSprite3D").Play("Side");
+            sprite.FlipH = true;
+            sprite.Play("Side");
         }
         if (@event.IsActionPressed("Bump Power"))
         {
@@ -101,21 +112,21 @@ public partial class Player : Node3D
         {
             if (Input.IsActionPressed("Forward"))
             {
-                GetNode<AnimatedSprite3D>("AnimatedSprite3D").Play("Forward");
+                sprite.Play("Forward");
             }
             if (Input.IsActionPressed("Backward"))
             {
-                GetNode<AnimatedSprite3D>("AnimatedSprite3D").Play("Backward");
+                sprite.Play("Backward");
             }
             if (Input.IsActionPressed("Right"))
             {
-                GetNode<AnimatedSprite3D>("AnimatedSprite3D").FlipH = false;
-                GetNode<AnimatedSprite3D>("AnimatedSprite3D").Play("Side");
+                sprite.FlipH = false;
+                sprite.Play("Side");
             }
             if (Input.IsActionPressed("Left"))
             {
-                GetNode<AnimatedSprite3D>("AnimatedSprite3D").FlipH = true;
-                GetNode<AnimatedSprite3D>("AnimatedSprite3D").Play("Side");
+                sprite.FlipH = true;
+                sprite.Play("Side");
             }
         }
         if (!GetNode<Timer>("AttackCooldown").IsStopped() || attacking)
